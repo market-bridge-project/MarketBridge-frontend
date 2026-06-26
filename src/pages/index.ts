ee612fa7f3620c62c0918a27ae843f386860efd1
lyn0719 <usolnim@naver.com>
2026-06-27 02:26:47 +0900

    feat: 가게 목록 페이지 생성 및 라우터 설정

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MapPage } from './MapPage'
-export { default as SearchPage } from './RecommendPage'
-export { default as RecommendResult } from './RecommendResultPage'
-export { default as StoreDetail } from './StoreDetailPage'
